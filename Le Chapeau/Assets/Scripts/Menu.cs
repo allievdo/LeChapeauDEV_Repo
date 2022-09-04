1b02a21afc49ff5ce0cf0eede327451aea14a249
allikitten <98057200+allikitten@users.noreply.github.com>
2022-09-04 15:06:04 -0700

    WebBuild

--- Le Chapeau/Assets/Scripts/Menu.cs
+++ Le Chapeau/Assets/Scripts/Menu.cs
@@ -6,8 +6,93 @@ using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class Menu : MonoBehaviour
+public class Menu : MonoBehaviourPunCallbacks
 {
     [Header("Screens")]
+    public GameObject mainScreen;
+    public GameObject lobbyScreen;
 
+    [Header("Main Screen")]
+    public Button createRoomButton;
+    public Button joinRoomButton;
+
+    [Header("Lobby Screen")]
+    public TextMeshProUGUI playerListText;
+    public Button startGameButton;
+
+    void Start()
+    {
+        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        createRoomButton.interactable = true;
+        joinRoomButton.interactable = true;
+    }
+
+    void SetScreen (GameObject screen)
+    {
+        mainScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+
+        screen.SetActive(true);
+    }
+
+    public void OnCreateRoomButton (TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.CreateRoom(roomNameInput.text);
+    }
+
+    public void OnJoinRoomButton (TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.JoinRoom(roomNameInput.text);
+    }
+
+    public void OnPlayerNameUpdate (TMP_InputField playerNameInput)
+    {
+        PhotonNetwork.NickName = playerNameInput.text;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SetScreen(lobbyScreen);
+
+        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        updateLobbyUI();
+    }
+
+    [PunRPC]
+    public void updateLobbyUI()
+    {
+        playerListText.text = "";
+
+        //display all players currently in the lobby
+        foreach(Player player in PhotonNetwork.PlayerList)
+        {
+            playerListText.text += player.NickName + "\n";
+        }
+
+        //only the host can start the game
+        if (PhotonNetwork.IsMasterClient)
+            startGameButton.interactable = true;
+        else
+            startGameButton.interactable = false;
+    }
+
+    public void OnLeaveLobbyButton()
+    {
+        PhotonNetwork.LeaveRoom();
+        SetScreen(mainScreen);
+    }
+
+    public void OnStartGameButton()
+    {
+        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
+    }
 }
